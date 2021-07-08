f49d14760c51b776d3f5204b9229b5ba1f024299
akagaki <taka.a.0401@gmail.com>
2021-07-09 00:25:20 +0900

    feat teampage

---
+++ src/resources/views/team/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('content')
+   <p>ここが本文のコンテンツです。</p>
+   <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+copyright 2021 akagaki.
+@endsection
